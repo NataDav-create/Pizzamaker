e90cb4e2cc532617cf89e5ba9a90e9079585e6ce
Natalia Davydova <davydovanatalia063@gmail.com>
2021-02-13 20:12:22 -0800

    add data-value for ingredients and drinks in index.html and function calculateTotal in main.js

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ const subject = document.querySelector('.modal-window__subject'),
     ingredientsSpan = document.querySelector('.modal-window__ingredients'),
     drinksSpan = document.querySelector('.modal-window__drinks');
 
+// let startPrice = 4.07;
+
 
 /* Add ingredients to pizza*/
 const addIngredients = () => {
@@ -21,6 +23,10 @@ const addIngredients = () => {
                     ingredient.classList.toggle('active');
                 }
             })
+            e.target.closest('.container-custom-checkbox').classList.toggle('active');
+            let choosenIngredients = [...document.querySelectorAll('.container-custom-checkbox.active')];
+            calculateTotal();
+
         })
     })
 }
@@ -32,8 +38,27 @@ const addDrinks = () => {
         drink.addEventListener('click', e => {
             let target = e.target.closest('.select-drink-item');
             target.classList.toggle('active');
+            let choosenDrinks = [...document.querySelectorAll('.select-drink-item.active')];
+            calculateTotal();
         })
     })
 }
-
 addDrinks();
+
+//calculate total
+const calculateTotal = () => {
+    let startPrice = 4.07;
+    let choosenIngredients = [...document.querySelectorAll('.container-custom-checkbox.active')],
+        choosenDrinks = [...document.querySelectorAll('.select-drink-item.active')];
+    const chooseActive = (el) => {
+        el.forEach(item => {
+            let totalOfIngredients = Number(item.dataset.value);
+            console.log(totalOfIngredients);
+            startPrice += totalOfIngredients;
+            return startPrice;
+        })
+    }
+    chooseActive(choosenIngredients);
+    chooseActive(choosenDrinks);
+    totalAmount.innerHTML = `${startPrice.toFixed(2)}$`
+}
