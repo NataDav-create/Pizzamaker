ae92b33e79367b9688ee164540059c27e4a15a30
Natalia Davydova <davydovanatalia063@gmail.com>
2021-02-13 17:27:42 -0800

    add index.html with markup, add style and add variables in main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const inputsCheckbox = document.querySelectorAll('.container-custom-checkbox input'),
+    ingredients = document.querySelectorAll('.current-pizza-item'),
+    drinks = document.querySelectorAll('.select-drink-item'),
+    totalAmount = document.querySelector('.total-amount>.summa'),
+    orderBtn = document.querySelector('.typical-btn'),
+    modalWindow = document.querySelector('.modal-window'),
+    submitBtn = document.querySelector('.modal-window__submit-btn');
+
+const subject = document.querySelector('.modal-window__subject'),
+    ingredientsSpan = document.querySelector('.modal-window__ingredients'),
+    drinksSpan = document.querySelector('.modal-window__drinks');
