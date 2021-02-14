757404b280647d608d26d0bc4b659c3e9fd198c9
Natalia Davydova <davydovanatalia063@gmail.com>
2021-02-13 18:42:29 -0800

    add data-name for ingredients in html and add function addIngredients and addDrinks in main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const inputsCheckbox = document.querySelectorAll('.container-custom-checkbox input'),
-    ingredients = document.querySelectorAll('.current-pizza-item'),
-    drinks = document.querySelectorAll('.select-drink-item'),
+const inputsCheckbox = [...document.querySelectorAll('.container-custom-checkbox input')],
+    ingredients = [...document.querySelectorAll('.current-pizza-item')],
+    drinks = [...document.querySelectorAll('.select-drink-item')],
     totalAmount = document.querySelector('.total-amount>.summa'),
     orderBtn = document.querySelector('.typical-btn'),
     modalWindow = document.querySelector('.modal-window'),
@@ -9,3 +9,31 @@ const inputsCheckbox = document.querySelectorAll('.container-custom-checkbox inp
 const subject = document.querySelector('.modal-window__subject'),
     ingredientsSpan = document.querySelector('.modal-window__ingredients'),
     drinksSpan = document.querySelector('.modal-window__drinks');
+
+
+/* Add ingredients to pizza*/
+const addIngredients = () => {
+    inputsCheckbox.forEach(input => {
+        input.addEventListener('click', e => {
+            let target = e.target.closest('.container-custom-checkbox').dataset.name;
+            ingredients.forEach(ingredient => {
+                if (ingredient.classList.contains(target)) {
+                    ingredient.classList.toggle('active');
+                }
+            })
+        })
+    })
+}
+addIngredients();
+
+//add drinks
+const addDrinks = () => {
+    drinks.forEach(drink => {
+        drink.addEventListener('click', e => {
+            let target = e.target.closest('.select-drink-item');
+            target.classList.toggle('active');
+        })
+    })
+}
+
+addDrinks();
